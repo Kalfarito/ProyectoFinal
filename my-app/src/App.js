bed72e81d86005d3bf057b18421a60b8ce0b90c8
t0n100 <145083553+t0n100@users.noreply.github.com>
2023-12-02 16:54:47 -0600

    main y user
    se crearon el main y el user  y los metodos de insertar y editar  aun no se sabe se funcionan

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,10 @@ import Home from "./components/home/home";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
 import Note from "./components/Note/Note"
+import Main from './components/main/main';
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="Login" element={<Login />} />
+            <Route path="main" element={<Main />} />
             <Route path="SignUp" element={<SignUp />} />
             <Route path="Note" element={<Note />} />
           </Routes>
