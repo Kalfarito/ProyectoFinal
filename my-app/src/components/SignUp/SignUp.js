9218d4c0441c0f6a34c631fa6c9ddda12aab9ed9
Kalfarito <keil8ralfaro2002@gmail.com>
2023-12-05 14:23:23 -0600

    SignUp Funcional
    SignUp Funcional

--- my-app/src/components/SignUp/SignUp.js
+++ my-app/src/components/SignUp/SignUp.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,122 +11,151 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+const Registro = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    lastnames: '',
+    email: '',
+    username: '',
+    password: '',
+  });
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
-export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
+  const reset = () => {
+    setFormData({
+      primernombre: '',
+      lastnames: '',
+      email: '',
+      username: '',
+      password: '',
+    });
+  };
+  
+
+  const urlDelApi = "http://localhost:8080/api/newuser";
+  const handleSubmit = (e) => {
+    axios
+
+      .post(urlDelApi,formData,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+      }
+      ) 
+      .then((response) => {
+       
+        console.log(response.data);
+
+        console.log(formData);
+        window.location.href="../Login/"
+      })
+      .catch((error) => {
+       
+        console.error(error);
+       
+      })
+      .finally(() => {
+      });
+  };
+
+
+  const defaultTheme = createTheme();
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="primernombre"
-                  required
-                  fullWidth
-                  id="primernombre"
-                  label="Primer Nombre"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="apellidos"
-                  label="Apellidos"
-                  name="apellidos"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Correo electronico"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="contraseña"
-                  label="Contraseña"
-                  type="password"
-                  id="contraseña"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="Desea recebir notificaciones de nuevas actualizaciones"
-                />
-              </Grid>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="given-name"
+                name="primernombre"
+                required
+                fullWidth
+                id="primernombre"
+                label="Primer Nombre"
+                autoFocus
+              />
             </Grid>
-            <Button 
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Crear
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="Login" variant="body2">
-                  Login
-                </Link>
-              </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id="apellidos"
+                label="Apellidos"
+                name="apellidos"
+                autoComplete="family-name"
+              />
             </Grid>
-          </Box>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Correo electronico"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="contraseña"
+                label="Contraseña"
+                type="password"
+                id="contraseña"
+                autoComplete="new-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="Desea recebir notificaciones de nuevas actualizaciones"
+              />
+            </Grid>
+          </Grid>
+          <Button 
+            name="btnRegistrar"
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleSubmit}>
+            Crear
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+            </Grid>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
+      </Box>
+      <p>Si ya tiene una cuenta, haga <a href="./login">click aquí</a> para ingresar.</p>
+    </Container>
+  </ThemeProvider>
   );
-}
+};
+export default Registro;
