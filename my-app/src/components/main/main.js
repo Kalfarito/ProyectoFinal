d47d8c49420ea728e076bf422fea0fc3837e005d
Kalfarito <keil8ralfaro2002@gmail.com>
2023-12-05 15:45:17 -0600

    Menu Principal barra y Mocks
    Menu Principal barra y Mocks

--- my-app/src/components/main/main.js
+++ my-app/src/components/main/main.js
@@ -1,205 +1,179 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styles from "./main.module.css"
+import React, {useEffect} from 'react';
+import PropTypes from 'prop-types';
+import styles from './main.module.css';
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
-import { useState } from "react";
-import axios from "axios";
-import User from "../User/User";
 import Note from "../Note/Note";
-
-const Main = () => {
-  //constante
-  const [data, setData] = React.useState(
-    "Tarea de insertar notas en la base de datos "
-  );
-  const [formValues, setFormValues] = React.useState();
-  const [users, setUsers] = React.useState();
-  const [notes, setNotes] = React.useState();
-
-  const urlDelApi = "";
-
-
-  const onChancheInput = (event) => {
-    let name = event.target.name;
-    let value = event.target.value;
-    console.log(event);
-    console.log(name);
-    console.log(value);
-
-    setFormValues({ ...formValues, [name]: value });
-  };
-
-  const callAPINotes = (event) => {
-    axios
-      .get(`${urlDelApi}/notas`) //cambio direccion
-      .then(function (response) {
-        // handle success
-        console.log(response);
-        console.log(response.data.records);
-        console.log(response.statusText);
-        setNotes(response.data.records);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      });
-  };
-
-  const clearNotes = (event) => {
-    setNotes();
-  };
+import axios from "axios";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import {TextareaAutosize } from '@mui/material' ;
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 
-/* tarea de insertar en el api */
-    const [formData, setFormData] = useState({
-      UserID: '',
-      Title: '',
-      Content: ''
-    });
-    const handleChange = (e) => {
-      setFormData({
-        ...formData,
-        [e.target.name]: e.target.value
-      });
+
+const Main = (props) => 
+  {
+  
+    const [formValues, setFormValues] = React.useState();
+    const [authenticated, setAuthenticated] = React.useState();
+    const [users, setUsers] = React.useState();
+    const [notes, setNotes] = React.useState();
+    const [user,setUser]= React.useState(props.user);
+  
+    const urlDelApi = "http://localhost:8080/api/note/all";
+  
+    useEffect(()=>{
+      const user=localStorage.getItem("user"); 
+      setUser(props.user);
+    },[])
+
+    const mockNotes = [
+      {
+        NoteID: 1,
+        UserID: 1,
+        Title: "Nota 1",
+        Content: "Prueba de nota local1",
+        CreatedAt: "2023-10-10 17:43:12",
+      },
+      {
+        NoteID: 2,
+        UserID: 1,
+        Title: "nota 2",
+        Content: "Prueba de nota local2",
+        CreatedAt: "2023-10-10 17:43:12",
+      },
+     
+    ];
+    
+    const onChancheInput = (event) => {
+      let name = event.target.name;
+      let value = event.target.value;
+      console.log(event);
+      console.log(name);
+      console.log(value);
+  
+      setFormValues({ ...formValues, [name]: value });
     };
-
-
-    const handleReset = () => {
-      setFormData({
-        UserID: '',
-        Title: '',
-        Content: ''
-      });
-    };
-
-    const insertNoteToDB2 = () => {
+    const callAPINotes = (event) => {
+      const params = {
+        id: '1',
+      };
       axios
-      .post(`${urlDelApi}/notas`, formData)
-      .then(function (response) {
-       
-        handleReset();
-      })
+        .get(`${urlDelApi}`,{params},  {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            // Otros encabezados si son necesarios
+          },
+        })
         .then(function (response) {
-          
-          callAPINotes();
+          console.log(response);
+          console.log(response.data.records);
+          console.log(response.statusText);
+          setNotes(response.data.records);
         })
         .catch(function (error) {
-          
           console.log(error);
-        })
-        .finally(function () {
-          
         });
     };
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      insertNoteToDB2(formData);
+    
+    const callAPMockNotes = (event) => {
+      setNotes(mockNotes);
+      setNotes([...mockNotes]);
     };
-  return (
-    <div className={styles.Main}>
-      <h1>{data}</h1>
-      <Grid
-        container
-        spacing={2}
-        style={{
-          inset: 0,
-          margin: "auto",
-          textAlign: "center",
-          maxWidth: "80%",
-        }}
-      >
-        <Grid item xs={6}>
+   
+    const clearNotes = (event) => {
+      setNotes();
+    };
+   
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
   
-          <Grid item xs={3} style={{}}>
-          <form onSubmit={handleSubmit}>
-        <TextField
-             type="text"
-             name="UserID"
-            required
-            label="UserID"
-            onChange={handleChange}
-            variant="standard"
-          />
-        <br />
+   
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Menú Principal
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <h1>Bienvenido {user?.usuario}</h1>
 
-<TextField
-            type="text"
-            name="Title"
-            value={formData.Title}
-            onChange={handleChange}
-            label="Titulo"
-            variant="standard"
-          />
-<br />
-
-<TextField
-           name="Content"
-           value={formData.Content}
-           onChange={handleChange}
-            label="informacion "
-            variant="outlined"
-            
-          />
-<br /><br />
-<Button  type="submit" variant="contained"color="success" sx={{ mx: 2 }}>
-            Insertar
-          </Button>
-          </form>
+      <Grid
+          container
+          spacing={2}
+          style={{
+            inset: 0,
+            margin: "auto",
+            textAlign: "center",
+            maxWidth: "80%",
+          }}
+        >
+          <Grid item xs={12}>
+            {/* ... */} 
           </Grid>
-
-          <Button onClick={callAPINotes} variant="contained" sx={{ mx: 3 }}>
-            Llamar API
-          </Button>
-          <br></br><br></br>
-          <Button onClick={clearNotes} color="secondary" variant="text">
-            Limpiar
-          </Button>
+          <Grid item xs={6}>
+          <h1> Blog de Notas</h1>
+            <Button onClick={callAPMockNotes} variant="contained" sx={{ mx: 2 }}>
+              Ver notas
+            </Button>
+            <Button onClick={clearNotes} color="secondary" variant="text">
+              Ocultar
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={6} style={{}}>
-          <h2>Llamar Local y base de datos</h2>
-          <p>
-            {" "}
-           El boton llama a MockNotes 
-          </p>
-          <Button onClick={clearNotes} color="secondary" variant="text">
-            Limpiar
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          {" "}
-          <p>
-            
-          </p>
-        </Grid>
-      </Grid>
-
-      <Card id="card-main" className={styles["card-main"]}>
-        {console.log("por aca ando")}
-
-        <Grid container spacing={2}>
-          {notes?.map((nota, index) => {
-            return (
-              <Grid item xs={4}>
-                {" "}
-                <Note titulo="titulo" note={nota}></Note>
+        <br></br>
+        <br></br>
+        <br></br>
+        <Card id="card-home" className={styles["card-home"]}>
+          <Grid container spacing={4}>
+            {notes?.map((nota, index) => 
+            (
+              <Grid item xs={6} key={index}>
+                  <Note titulo="titulo" note={nota}>  
+                  </Note>
               </Grid>
-            );
-          })}
-        </Grid>
-      </Card>
-    </div>
+            ))}
+          </Grid>
+        </Card>
+    </Box>
   );
-};
+}
+
 
 Main.propTypes = {};
 
 Main.defaultProps = {};
 
 export default Main;
+
+
+
+
+
+
