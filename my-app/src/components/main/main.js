200e18fe5e134dfa141fff500b1409e59114508d
Kalfarito <keil8ralfaro2002@gmail.com>
2023-12-11 16:32:02 -0600

    VistaNotas mejorada
    VistaNotas mejorada

--- my-app/src/components/main/main.js
+++ my-app/src/components/main/main.js
@@ -7,17 +7,19 @@ import MenuItem from '@mui/material/MenuItem';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 
 const NotasId = () => {
   const [notes, setNotes] = useState([]);
   const [showNotes, setShowNotes] = useState(false);
+  const [hovered, setHovered] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const urlDelApi = "http://localhost:8080/api/all/Notes";
         const params = {
-          id: '1',   // userId: userId, // Suponiendo que el parámetro esperado por la API es userId
+          id: '1',
         };
         const response = await axios.get(urlDelApi, { params });
         setNotes(response.data);
@@ -39,23 +41,20 @@ const NotasId = () => {
     setShowNotes(false);
   };
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleHoverEnter = () => {
+    setHovered(true);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleHoverLeave = () => {
+    setHovered(false);
   };
 
   return (
     <div data-testid="Registro">
-      <AppBar position="static">
+      <AppBar position="static" sx={{ background: '#1976D2' }}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-
+            <span style={{ color: 'white' }}>Notes App</span>
           </Typography>
           <Button color="inherit" onClick={() => window.location.href = "../note"}>
             Nueva nota
@@ -69,26 +68,52 @@ const NotasId = () => {
         </Toolbar>
       </AppBar>
 
-      <h1>Notas por id de usuario</h1>
+      <h1>Todas las notas</h1>
 
-      <Button variant="contained" onClick={handleShowNotes}>
+      <Button variant="contained" onClick={handleShowNotes} sx={{ backgroundColor: '#1976D2' }}>
         Mostrar Notas
       </Button>
-      <Button variant="contained" onClick={handleHideNotes}>
+      <Button variant="contained" onClick={handleHideNotes} sx={{ backgroundColor: '#1976D2' }}>
         Ocultar Notas
       </Button>
 
       {showNotes && (
         <Grid container spacing={2}>
-          {notes.map((note) => (
+          {notes.map((note, index) => (
             <Grid item key={note.id} xs={12} sm={6} md={4}>
-              <div>
-                <h3>{note.title}</h3>
-                <p>{note.content}</p>
-              </div>
+              <Paper elevation={3} style={{ padding: '15px', marginBottom: '15px' }}>
+                <Typography variant="h5" sx={{ fontWeight: 'bold', marginBottom: '10px', color: 'black' }}>Nota {index + 1}</Typography>
+                <Typography variant="h6" sx={{ color: 'black' }}>{note.title}</Typography>
+                <Typography sx={{ color: 'black' }}>{note.content}</Typography>
+              </Paper>
             </Grid>
           ))}
         </Grid>
+      )}
+
+      {!showNotes && hovered && (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <p
+            style={{
+              cursor: 'pointer',
+              fontSize: '24px',
+              fontWeight: 'bold',
+              color: 'black',
+              textDecoration: 'underline',
+            }}
+            onClick={handleShowNotes}
+            onMouseEnter={handleHoverEnter}
+            onMouseLeave={handleHoverLeave}
+          >
+            Mostrar Notas
+          </p>
+        </div>
+      )}
+
+      {!showNotes && !hovered && (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <p style={{ fontSize: '28px', fontWeight: 'bold', color: 'black' }}>Las notas están ocultas.</p>
+        </div>
       )}
     </div>
   );
